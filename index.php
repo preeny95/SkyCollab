249b1cb64a3679cea0f9d487fdc8c393b812129c
preeny95 <npr01@sbgml02459.sbg.skybettingandgaming.com>
2017-09-15 10:27:23 +0100

    1st

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<html>
+<head>
+    <title>Super Arrows</title>
+</head>
+<body>
+    <h1>Welcome to Super Arrows!</h1>
+
+    <form method="POST" action="results.php">
+        <?php 
+
+        
+            //change to 2d array
+            $allGames = array(array("James Smith", "Luke Green"), array("Phil Taylor", "Michael Van Gerwin"));
+
+            foreach($allGames as $game)
+            {
+                echo "<p>".$game[0]. " vs " .$game[1]. "</p>";
+                echo "<input type='number' name='player1[]'/>";
+                echo "<input type='number' name='player2[]'/>";
+            }
+
+            echo "<br/><br/>";
+            echo "<input type='submit'/>";
+            
+        ?>
+    </form>
+</body>
+</html>
